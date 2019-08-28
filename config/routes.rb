aa59f73225856d5ccd5bddddc505d61054d20005
palomagabic <p.corteslazo@gmail.com>
2019-08-28 01:00:51 -0400

    ya termine el desafio

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
+  root 'pages#home'
   get 'pages/home'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
